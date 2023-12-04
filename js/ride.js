a7f36592c4f4798ff77395a7f79d9e4633f3b245
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2023-12-03 18:44:50 -0600

    home page done-ish

--- js/ride.js
+++ js/ride.js
@@ -185,3 +185,21 @@ function displayUpdate(text, color='green') {
 }
 
 
+async function displayText(){
+    var text = "yo yo yo this is some text!!!"
+    //document.getElementById("textField").innerHTML = text;
+    //text.style.display = "block";
+
+    var response = await fetch("https://api.chucknorris.io/jokes/random", {
+        method: "GET" // default, so we can ignore
+    })
+    var exam = await response.json();
+    
+    var poop = JSON.stringify(exam, undefined, 2);
+    var obj = JSON.parse(poop);
+    var values = Object.values(obj);
+
+    document.getElementById("textField").textContent = obj.value;
+    text.style.display = "block";
+
+}
