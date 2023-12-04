b6553b70882bed4595edf027a076ab202f59263c
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2023-12-03 18:16:04 -0600

    chuck norris

--- js/ride.js
+++ js/ride.js
@@ -180,16 +180,3 @@ function displayUpdate(text, color='green') {
 }
 
 
-async function displayText(){
-    var text = "yo yo yo this is some text!!!"
-    //document.getElementById("textField").innerHTML = text;
-    //text.style.display = "block";
-
-    var response = await fetch("https://api.chucknorris.io/jokes/random", {
-        method: "GET" // default, so we can ignore
-    })
-    var exam = await response.json();
-    document.getElementById("textField").textContent = JSON.stringify(exam, undefined, 2);
-    text.style.display = "block";
-
-}
