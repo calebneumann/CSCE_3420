936090224eeb4d82b3e300418a9baff211a550b6
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2023-12-04 19:33:15 -0600

    finally got image working

--- js/ride.js
+++ js/ride.js
@@ -227,9 +227,7 @@ async function displayText(){
 
     forecastString = JSON.stringify(result.properties.periods[0].icon, undefined, 2);
     var iconData = JSON.parse(forecastString);
-    var nullIconData = "";
-    document.getElementById("icon").textContent = nullIconData;
-    objForecast = '\0';
+
     
     var img = document.createElement("img"); 
     img.src = iconData; 
